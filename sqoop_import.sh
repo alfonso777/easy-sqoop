fb7792632ceb3924243b42a35bd6274d9fce9cc8
Victoriano Alfonso Phocco Diaz <victoriano.alfonso@semantix.com.br>
2019-09-29 22:31:03 -0300

    Implementation of partitioned mode ingestion. Adjusting the two modes to be used transparently.

--- sqoop_import.sh
+++ sqoop_import.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 function checking() 
 {
   if [[ -z $env || -z $sourcetable ]]; then
     echo 'One or more variables are undefined. Required options: --env, --sourcetable.'
     echo 'Optional options: --mode, --splitby, --hivetable, --format, --extraoptions.'
     echo 'Remarks:'
-    echo '  - Valid mode values: FULL '
+    echo '  - Valid mode values: FULL or PARTIIONED. FULL is a default value.'
     echo '  - Supported formats: parquetfile, textfile, sequencefile and avrodatafile.'  
+    echo '  - The --partitionkey and --partitionvalue options are required when the mode is PARTIIONED.'
     echo '  - The default format is parquet'
     echo '  - The --extraoptions option is useful to pass sqoop commands.'
     echo '    Example: --extraoptions "--map-column-java column1=String,column2=Long --where \"id<123456\" "'
@@ -15,6 +15,24 @@ function checking()
     echo 'sh sqoop-import.sh --env cerner --sourcetable table1'
     echo 'sh sqoop-import.sh --env dwhiae --sourcetable tableabc --hivetable tableabc1'
     echo 'sh sqoop-import.sh --env cerner --sourcetable tableabc --splitby column1 --extraoptions "--map-column-java column1=String,column2=Long"'
+    echo 'sh sqoop-import.sh --env cerner --mode PARTITIONED --sourcetable table1 --partitionkey dataingestion --partitionvalue 2019-06-07'
+    exit 1
+  fi
+
+  if [[ $MODE != "PARTITIONED" && $MODE != "FULL" ]]; then
+    echo "Mode $MODE not supported, use PARTITIONED or FULL (default if not defined) instead."
+    exit 1
+  fi
+
+  if [[ $MODE == "PARTITIONED" && (-z $partitionkey || -z $partitionvalue) ]]; then
+    echo "ERROR: In partitioned mode, the --partitionkey or --partitionvalue are required."
+    echo "Example: "
+    echo "sh sqoop-import.sh --env cerner --mode PARTITIONED --sourcetable table1 --partitionkey dataingestion --partitionvalue 2019-06-07'"
+    exit 1
+  fi
+
+  if [[ $MODE == "FULL" && ( $partitionkey ||$partitionvalue) ]]; then
+    echo "WARNING: In full mode, the --partitionkey or --partitionvalue are not required."
     exit 1
   fi
 
@@ -32,6 +50,7 @@ function print_configuration()
   echo "Table origin: $TABLE_ORIGIN "
   echo "Target database and table: $DB_TARGET.$TARGET_TABLE "
   echo "Split by: $SPLIT_BY_OPTION "
+  echo "Partition Key: $partitionkey, Partition Value: $partitionvalue "
   echo "Format and compress: $FORMAT_COMMAND $COMPRESS_COMMAND "
   echo "Additional Sqoop options: $extraoptions "
 }
@@ -46,6 +65,25 @@ function full_import()
   $SPLIT_BY_OPTION \
   --hive-database $DB_TARGET  --hive-table $TARGET_TABLE \
   --hive-overwrite --hive-import --num-mappers $NUM_MAPPERS \
+  $FORMAT_COMMAND \
+  $COMPRESS_COMMAND \
+  $extraoptions \
+  --hive-drop-import-delims > sqoop_${sourcetable,,}.out
+}
+
+function partitioned_import()
+{
+  sqoop import -Dhadoop.security.credential.provider.path=$HADOOP_KEYSTORE \
+  --connect $CONNECTION \
+  --username $USERNAME \
+  --password-alias $CREDENTIAL_ALIAS \
+  --table $TABLE_ORIGIN \
+  $SPLIT_BY_OPTION \
+  --hcatalog-database $DB_TARGET  --hcatalog-table $TARGET_TABLE \
+  --num-mappers $NUM_MAPPERS \
+  --hcatalog-partition-keys $partitionkey \
+  --hcatalog-partition-values $partitionvalue \
+  --delete-target-dir \
   $FORMAT_COMMAND \
   $COMPRESS_COMMAND \
   $extraoptions \
@@ -89,5 +127,12 @@ checking
 setup
 
 #Executing
-echo 'Executing full import ...'
-full_import
+if [[ $MODE == "FULL" ]]; then
+  echo 'Executing full import ...'
+  full_import
+elif [[ $MODE == "PARTITIONED" ]]; then
+  echo 'Executing import wuth partitioning ...'
+  partitioned_import 
+else  
+  echo "Mode $MODE not supported, use PARTITIONED or FULL(default if not definied) instead."
+fi
